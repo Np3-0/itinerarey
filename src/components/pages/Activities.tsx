fc0b3a1391bfb883a6d8fde89b5c08c3f8bdf2be
Nate-OBrien <nathan.obrien@mcvts.org>
2026-09-20 23:52:59 -0400

    base activity page layout + ai route

--- src/components/pages/Activities.tsx
+++ src/components/pages/Activities.tsx
@@ -1,5 +1,74 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { type cookieData, getCookie, saveCookie } from "../../utils/cookies.ts";
+import Navbar from "../partials/Navbar.tsx";
+import { getResponseFromAI } from "../../utils/AI";
+import Button from "../partials/Button.tsx";
+
 export default function Activities() {
+    const navigate = useNavigate();
+    const [cookieData, setCookieData] = useState<cookieData | null>(null);
+    const [activities, setActivities] = useState<any | null>(null);
+    const [selectedActivities, setSelectedActivities] = useState<any | null>(null);
+
+    useEffect(() => {
+        const fetchActivities = async () => {
+            const cookie = getCookie("tripInfo");
+            if (!cookie) {
+                navigate("/");
+                return;
+            }
+            setCookieData(cookie);
+
+            const AIResult = await getResponseFromAI(cookie);
+            if (AIResult.length === 0) {
+                alert("No activities were found. Please change your search parameters.")
+                navigate("/plan");
+                return;
+            }
+            setActivities(AIResult);
+        };
+
+        fetchActivities();
+    }, [navigate]);
+
+    const handleActivitySubmission = () => {
+
+    }
+
     return (
-        <h1>the activities will be here one day.</h1>
+        <>
+            <Navbar />
+            <div className="flex flex-col items-center justify-center min-h-screen bg-cerulean">
+                <h1 className="text-4xl font-bold text-heading text-white mt-18">Activities</h1>
+                <p className="text-xl font-semibold text-heading text-white my-6">Please select as many activities as you would like!</p>
+
+                {selectedActivities && (
+                    <Button text="Continue" onClick={() => { handleActivitySubmission() }} colorway="primary" />
+                )}
+                {activities ? (
+                    <div className="mt-6 w-full max-w-4xl bg-accent-blue rounded-lg shadow-md p-6">
+                        <h1 className="text-2xl font-semibold text-heading mb-4 text-white">Available activities: {activities.length}</h1>
+                        {activities.length === 0 ? (
+                            <>
+                                <p className="text-lg text-white mt-4">No activities available for the selected dates. Please change your planned information.</p>
+                                <button
+                                    className="bg-floral-white text-cerulean hover:scale-110 py-3 px-12 rounded-full transition duration-300 font-semibold text-lg cursor-pointer"
+                                    onClick={() => navigate("/plan")}
+                                >
+                                    Go back
+                                </button>
+                            </>
+                        ) : (
+                            <>
+                                /* Add activity mapping here. */
+                            </>
+                        )}
+                    </div>
+                ) : (
+                    <p className="text-lg text-white mt-4">Loading activity0 data... This might take a minute!</p>
+                )}
+            </div>
+        </>
     )
 }
---
+++ src/utils/AI.ts
@@ -0,0 +1,38 @@
+import { GoogleGenAI } from "@google/genai";
+import * as z from "zod";
+import type { cookieData } from "./cookies.ts";
+
+export async function getResponseFromAI(cookieData: cookieData ) {
+    const ai = new GoogleGenAI({apiKey: import.meta.env.VITE_GEMINI_API_KEY});
+    const aiInput = `Location: ${cookieData.destination} | 
+                    Budget: ${cookieData.budgets.activity} | 
+                    Startdate: ${cookieData.dates.startDate} | 
+                    Enddate: ${cookieData.dates.endDate}`
+
+    const activityJSONSchema = z.object({
+        activity: z.string().describe("Name of the activity."),
+        location: z.string().describe("Address of the activity."),
+        desc:  z.string().describe("Description of the activity."),
+        price: z.number().describe("Price of the activity."),
+        additionalInfo: z.array(z.string()).describe("Anything additional about the activity.").optional(),
+    });
+    const activityListSchema = z.array(activityJSONSchema);
+    const fullSchema = z.toJSONSchema(activityListSchema);
+
+    const interaction = await ai.interactions.create({
+        model: "gemini-3.7-flash",
+        system_instruction: `You are an AI bot tasked with finding activities to do in specific places,
+            for a specific amount of days. Return a wide variety of activities, each with its price,
+            location, description, and any extra info. The total should fit within the given budget.`,
+        input: aiInput,
+        response_format: {
+            type: "text",
+            mime_type: "application/json",
+            schema: fullSchema
+        }
+    });
+
+    if (!interaction.output_text) return [];
+    const res = activityListSchema.parse(JSON.parse(interaction.output_text));
+    return res;
+}
